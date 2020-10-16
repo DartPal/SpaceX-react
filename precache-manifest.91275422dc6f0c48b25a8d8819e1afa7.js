6defd93a17cebb61d1a914ba4bec50f0f2716dc3
DartPal <kapustinpavel1998@mail.ru>
2020-10-16 23:21:12 +0300

    Updates

---
+++ precache-manifest.91275422dc6f0c48b25a8d8819e1afa7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a1614b19568e4a0f960acbe317477e",
+    "url": "/SpaceX-react/index.html"
+  },
+  {
+    "revision": "4eae1809d2d80538220b",
+    "url": "/SpaceX-react/static/css/main.2297a858.chunk.css"
+  },
+  {
+    "revision": "f35373f11449c41aed6c",
+    "url": "/SpaceX-react/static/js/2.629dd2a5.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/SpaceX-react/static/js/2.629dd2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eae1809d2d80538220b",
+    "url": "/SpaceX-react/static/js/main.721b3887.chunk.js"
+  },
+  {
+    "revision": "26acd2c80e9e3e66f59c",
+    "url": "/SpaceX-react/static/js/runtime-main.91d4e490.js"
+  },
+  {
+    "revision": "6160d2728a6ae1625a2e4bf7fbdc0e33",
+    "url": "/SpaceX-react/static/media/earth.6160d272.mp4"
+  },
+  {
+    "revision": "4e43f38ece9edcac9d2f6cf4a21e5ae1",
+    "url": "/SpaceX-react/static/media/falcon-1.4e43f38e.png"
+  },
+  {
+    "revision": "70c0d6ca57039f250f7a46da490536e7",
+    "url": "/SpaceX-react/static/media/falcon-9.70c0d6ca.png"
+  },
+  {
+    "revision": "c1e42c98b18c115c2219abbfd98b9dd4",
+    "url": "/SpaceX-react/static/media/falcon-heavy.c1e42c98.png"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/SpaceX-react/static/media/logo.d5ab31ce.svg"
+  },
+  {
+    "revision": "50a0c44519288f319f171b2250c40cb3",
+    "url": "/SpaceX-react/static/media/mars.50a0c445.mp4"
+  },
+  {
+    "revision": "c576729b41d649227eb2a4afff68ae39",
+    "url": "/SpaceX-react/static/media/moon.c576729b.mp4"
+  },
+  {
+    "revision": "7a69e90456d80e1fe0d1b4740e83dcf2",
+    "url": "/SpaceX-react/static/media/space.7a69e904.mp4"
+  },
+  {
+    "revision": "9502c3707c7d2debb7ddf433f6d28e39",
+    "url": "/SpaceX-react/static/media/starship.9502c370.png"
+  }
+]);
